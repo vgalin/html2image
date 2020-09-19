4658e8bbfd353391621388b2ecf7964dbbd035f9
vgalin <47885533+vgalin@users.noreply.github.com>
2020-09-19 20:09:48 +0200

    add --files and --urls support for the CLI

--- html2image/html2image_cli.py
+++ html2image/html2image_cli.py
@@ -10,13 +10,11 @@ from html2image import HtmlToImage
 
 def cli_entry():
 
-    print('CLI for html2image is not implemented yet.')
-
     parser = argparse.ArgumentParser()
 
     parser.add_argument('inputs', nargs='*')  # file.a, file.b, file.c
-    parser.add_argument('-u', '--urls', nargs='*', required=False)
-    parser.add_argument('-f', '--files', nargs='*', required=False)
+    parser.add_argument('-u', '--urls', nargs='*', required=False, default=[])
+    parser.add_argument('-f', '--files', nargs='*', required=False, default=[])
 
     parser.add_argument('-s', '--size', nargs=2, required=False)
     parser.add_argument('-n', '--name', required=False)  # screenshot.png
@@ -29,6 +27,7 @@ def cli_entry():
     parser.add_argument('--chrome_path', required=False)
     parser.add_argument('--firefox_path', required=False)
     parser.add_argument('--temp_path', required=False)
+    # todo: flag 'ask_for_filename' ?
 
     args = parser.parse_args()
 
@@ -38,7 +37,6 @@ def cli_entry():
         logging.basicConfig(level=logging.DEBUG)
     else:
         logging.basicConfig(level=logging.INFO)
-        # logging.basicConfig(filename='example.log',level=logging.DEBUG)
 
     logging.debug(f'{args = }')
 
@@ -47,9 +45,7 @@ def cli_entry():
     except Exception as e:
         logging.critical('Could not instanciate html2image.')
         logging.exception(e)
-        exit()
-    else:
-        logging.debug('html2Image has been correctly instanciated')
+        exit(1)
 
     if args.size:
         htmi.size = tuple(args.size)
@@ -57,31 +53,49 @@ def cli_entry():
     if args.output_path:
         htmi.output_path = args.output_path
 
+    # handling items that can be urls or files
     for item in args.inputs:
 
-        if item.startswith('http'):  # screen url
-            # todo output.name
+        if os.path.isfile(item):  # screen file
+            htmi.load_file(item)
+
+            # if item is not a css file, screen it
+            if not item.endswith('.css'):
+                htmi.screenshot(item, f'{item}.png')
+                logging.info(f'screened file \t{item} as {item}.png')
+
+        elif item.startswith('http'):  # screen url
+            # todo output name
             output_name = 'screenshot.png'
 
             htmi.screenshot_url(item, output_name)
-            logging.info(f'screened url \t{item}')
-
-        elif os.path.isfile(item):  # screen file
-            htmi.load_file(item)
-
-            if not item.endswith('.css'):
-                # if item is not a css file, screen it
-                htmi.screenshot(item, f'{item}.png')
-                logging.info(f'screened file \t{item}')
+            logging.info(f'screened url \t{item} as {output_name}')
 
         else:
             logging.error(f'invalid item \t{item}')
 
-    # if len(args.files) == 0:
-    #     args.files = None
+    for url in args.urls:
+        if not url.startswith('http'):
+            logging.info(f'adding missing protocol to {url}')
+            url = 'https://' + url
 
-    # if args.url is not None and args.files is not None:
-    #     print('todo')
+        # todo output name
+        output_name = 'screenshot_url.png'
+        htmi.screenshot_url(url, output_name)
+        logging.info(f'screened url \t{url} as {output_name}')
+
+    for file in args.files:
+        if not os.path.isfile(file):
+            logging.error(f'cannot find file {file}')
+            continue
+
+        htmi.load_file(file)
+        logging.info(f'loaded file \t{file}')
+
+        # if file is not a css file, screen it
+        if not file.endswith('.css'):
+            htmi.screenshot(file, f'{file}.png')
+            logging.info(f'screened file \t{file} as {file}.png')
 
 
 # todo delete
