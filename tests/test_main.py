2bea6bbf85e94af02625f3388a3a41799d223a2e
vgalin <47885533+vgalin@users.noreply.github.com>
2020-10-21 02:23:23 +0200

    redo basic testing following changes on screenshot method

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,63 +1,139 @@
 from html2image.main import HtmlToImage
-
 from PIL import Image
-import os
 
 OUTPUT_PATH = "tests_output"
-
-SIZE_LIST = (
-    (1920, 1080),
-    (1280, 720),
-    (800, 400),
-)
-
-
-def test_screenshot_file():
-    pass
-
-
-def test_screenshot_string():
-    hti = HtmlToImage(output_path=OUTPUT_PATH)
-
-    for size in SIZE_LIST:
-        img_name = f'fromstr_{size[0]}_{size[1]}.png'
-        img_relative_path = os.path.join(OUTPUT_PATH, img_name)
-
-        html_string = """\
-            <link rel="stylesheet" href="background.css">
-            <h1> An interesting title </h1>
-            Some interesting text
-        """
-
-        css_string = (
-            "body { background: linear-gradient(90deg, #00f 50%, #0f0 50%);}"
-        )
-
-        hti.load_str(html_string, as_filename='page.html')
-        hti.load_str(css_string, as_filename='background.css')
-        hti.screenshot('page.html', img_name, size=size)
-
-        img = Image.open(img_relative_path)
-        pixels = img.load()
-
-        assert size == img.size
-
-        # check colors at top corners
-        assert pixels[0, 0] == (0, 0, 255, 255)
-        assert pixels[size[1], 0] == (0, 255, 0, 255)
 
 
 def test_screenshot_url():
     hti = HtmlToImage(output_path=OUTPUT_PATH)
 
-    for size in SIZE_LIST:
-        img_name = f'pyorg_{size[0]}_{size[1]}.png'
-        img_relative_path = os.path.join(OUTPUT_PATH, img_name)
+    paths = hti.screenshot(url='https://www.python.org', save_as="pyorg.png")
+    img = Image.open(paths[0])
+    assert (1920, 1080) == img.size  # default size
 
-        hti.screenshot_url(
-            'https://www.python.org', img_name, size=size
-        )
 
-        img = Image.open(img_relative_path)
+def test_screenshot_multiple_urls():
+    hti = HtmlToImage(output_path=OUTPUT_PATH)
+    paths = hti.screenshot(
+        url=['https://www.python.org', "https://www.example.org/"],
+        save_as="mixed_urls.png",
+    )
 
-        assert size == img.size
+    for path in paths:
+        img = Image.open(path)
+        assert (1920, 1080) == img.size  # default size
+
+
+def test_screenshot_url_custom_size():
+    hti = HtmlToImage(output_path=OUTPUT_PATH)
+
+    test_size = (334, 485)
+
+    paths = hti.screenshot(
+        url='https://www.python.org',
+        save_as="pyorg_custom_size.png",
+        size=test_size,
+    )
+
+    img = Image.open(paths[0])
+    assert test_size == img.size  # default size
+
+
+def test_screenshot_url_custom_sizes():
+    hti = HtmlToImage(output_path=OUTPUT_PATH)
+
+    test_sizes = [
+        (100, 100),
+        (100, 1000),
+        (100, 200),
+    ]
+
+    paths = hti.screenshot(
+        url=[
+            'https://www.python.org',
+            'https://www.wikipedia.org/',
+            'https://www.example.org/',
+        ],
+        save_as="mixed_urls_custom_sizes.png",
+        size=test_sizes,
+    )
+
+    for wanted_size, path in zip(test_sizes, paths):
+        img = Image.open(path)
+        assert wanted_size == img.size  # default size
+
+
+def test_screenshot_url_sizes_missing_custom_names():
+    hti = HtmlToImage(output_path=OUTPUT_PATH)
+
+    test_sizes = [
+        (100, 100),
+        (100, 1000),
+    ]
+
+    paths = hti.screenshot(
+        url=[
+            'https://www.python.org',
+            'https://www.wikipedia.org/',
+            'https://www.example.org/',
+        ],
+        save_as=[
+            "python_100_100.png",
+            "wikipedia_100_1000.png",
+            "example_100_1000.png",
+        ],
+        size=test_sizes,
+    )
+
+    for wanted_size, path in zip(test_sizes, paths):
+        img = Image.open(path)
+        assert wanted_size == img.size  # default size
+
+
+def test_screenshot_string():
+    hti = HtmlToImage(output_path=OUTPUT_PATH)
+
+    html = "Hello"
+    css = "body{background: blue; font-size: 50px;}"
+
+    paths = hti.screenshot(html=html, css=css, save_as="blue_big_hello.png")
+
+    img = Image.open(paths[0])
+    pixels = img.load()
+
+    assert (1920, 1080) == img.size  # default size
+
+    # check colors at top left corner
+    assert pixels[0, 0] == (0, 0, 255, 255)  # blue + no transparency
+
+
+def test_screenshot_other_svg():
+    hti = HtmlToImage(output_path=OUTPUT_PATH)
+
+    paths = hti.screenshot(other='./examples/star.svg', save_as="star_svg.png")
+
+    img = Image.open(paths[0])
+    pixels = img.load()
+
+    assert (1920, 1080) == img.size  # default size
+
+    # check colors at top left corner
+    assert pixels[0, 0] == (0, 0, 0, 0)  # full transparency no color
+
+
+def test_screensshot_file():
+    hti = HtmlToImage(output_path=OUTPUT_PATH)
+
+    paths = hti.screenshot(
+        html="./examples/blue_page.html",
+        css="./examples/blue_background.css",
+        save_as="from_file.png",
+    )
+
+    img = Image.open(paths[0])
+    pixels = img.load()
+
+    assert (1920, 1080) == img.size  # default size
+
+    # check colors at top left corner
+    assert pixels[0, 0] == (0, 0, 255, 255)  # blue + no transparency
